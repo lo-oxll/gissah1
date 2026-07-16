21f9fe3f254cffafd7985c373b5a9929602cf344
ali kamil <43238145+lo-oxll@users.noreply.github.com>
2026-07-17 01:01:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -237,7 +237,7 @@ function openBookingModal(){
         {
           customer_name: name,
           phone_number: phone,
-          address: loc,
+          location: loc,
           product_name: `${p.name} (عدد: ${qty})`
         }
       ]);
@@ -257,7 +257,7 @@ function openBookingModal(){
       total,
       qty,
       customer_name: name,
-      address: loc,
+      location: loc,
       phone_number: phone,
       created_at: new Date().toISOString()
     };
